2648ec8a1e59499305b87f85c52523d7bd15b087
JuscelinoJunior <juniorjuscelino122@gmail.com>
2025-05-27 13:24:22 -0300

    feat: add GET by ID, POST, PUT and DELETE costumers

--- app/main.py
+++ app/main.py
@@ -17,3 +17,43 @@ def get_database_session():
 @app.get("/customers", response_model=list[schemas.CustomerResponse])
 def read_customers(skip: int = 0, limit: int = 100, database_session: Session = Depends(get_database_session)):
     return repositories.read_customers(database_session, skip, limit)
+
+@app.get("/customers/{customer_id}", response_model=schemas.CustomerResponse)
+def read_customers(customer_id: int, database_session: Session = Depends(get_database_session)):
+    retrieved_customer = repositories.read_customer(database_session, customer_id)
+
+    if not retrieved_customer:
+        raise HTTPException(status_code=400, detail="Customer not found")
+    return repositories.read_customer(database_session, customer_id)
+
+
+@app.post("/customers", response_model=schemas.CustomerResponse, status_code=status.HTTP_201_CREATED)
+def create_customer(customer: schemas.CustomerCreate, database_session: Session = Depends(get_database_session)):
+    # Check if already exists a user with same email
+    existing = repositories.read_customer_by_email(database_session, customer.email)
+    if existing:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return repositories.create_customer(database_session, customer)
+
+
+@app.put("/customers/{customer_id}", response_model=schemas.CustomerResponse)
+def update_customer(customer_id: int, customer: schemas.CustomerUpdate, database_session: Session = Depends(get_database_session)):
+    existing = repositories.read_customer(database_session, customer_id)
+    if not existing:
+        raise HTTPException(status_code=404, detail="Costumer not found")
+
+    if customer.email:
+        # Check if already exists a user with same email
+        other = repositories.read_customer_by_email(database_session, customer.email)
+        if other and other.id != customer_id:
+            raise HTTPException(status_code=400, detail="Email already registered")
+
+    return repositories.update_customer(database_session, customer_id, customer)
+
+
+@app.delete("/customers/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_customer(customer_id: int, database_session: Session = Depends(get_database_session)):
+    existing = repositories.read_customer(database_session, customer_id)
+    if not existing:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    repositories.delete_customer(database_session, customer_id)
--- app/repositories.py
+++ app/repositories.py
@@ -1,6 +1,42 @@
+from pydantic import EmailStr
 from sqlalchemy.orm import Session
 import models
+import schemas
 
 
 def read_customers(database_session: Session, skip: int = 0, limit: int = 100):
     return database_session.query(models.Customer).offset(skip).limit(limit).all()
+
+
+def read_customer_by_email(database_session: Session, email: EmailStr):
+    return database_session.query(models.Customer).filter(models.Customer.email == email).first()
+
+
+def read_customer(database_session: Session, customer_id: int):
+    return database_session.query(models.Customer).filter(models.Customer.id == customer_id).first()
+
+
+def create_customer(database_session: Session, customer: schemas.CustomerCreate):
+    customer_model = models.Customer(name=customer.name, email=customer.email)
+    database_session.add(customer_model)
+    database_session.commit()
+    database_session.refresh(customer_model)
+    return customer_model
+
+
+def update_customer(database_session: Session, customer_id: int, customer_data: schemas.CustomerUpdate):
+    customer_model = read_customer(database_session, customer_id)
+    if customer_data.name is not None:
+        customer_model.name = customer_data.name
+    if customer_data.email is not None:
+        customer_model.email = customer_data.email
+
+    database_session.commit()
+    database_session.refresh(customer_model)
+    return customer_model
+
+
+def delete_customer(database_session: Session, customer_id: int):
+    customer_model = read_customer(database_session, customer_id)
+    database_session.delete(customer_model)
+    database_session.commit()
